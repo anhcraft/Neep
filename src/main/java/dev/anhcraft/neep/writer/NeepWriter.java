71fc4ca4df5ff21a854198c6ad698f7e1f23b3c0
anhcraft <huynhduyanh123123@gmail.com>
2020-03-01 00:02:30 +0700

    added method to write to output stream

--- src/main/java/dev/anhcraft/neep/writer/NeepWriter.java
+++ src/main/java/dev/anhcraft/neep/writer/NeepWriter.java
@@ -1,9 +1,12 @@
 package dev.anhcraft.neep.writer;
 
-import dev.anhcraft.neep.errors.NeepReaderException;
 import dev.anhcraft.neep.errors.NeepWriterException;
 import dev.anhcraft.neep.struct.NeepSection;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class NeepWriter {
     @NotNull
@@ -11,5 +14,9 @@ public class NeepWriter {
         WriteContext readContext = new WriteContext(section, "");
         readContext.getWriteHandler().write();
         return readContext.buildString();
+    }
+
+    public static void write(@NotNull NeepSection section, @NotNull OutputStream outputStream) throws NeepWriterException, IOException {
+        outputStream.write(stringify(section).getBytes(StandardCharsets.UTF_8));
     }
 }
