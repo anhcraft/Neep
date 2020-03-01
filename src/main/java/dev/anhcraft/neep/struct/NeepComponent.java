38ee1aa16772d479f18a0f0415ac39d5de55b145
anhcraft <huynhduyanh123123@gmail.com>
2020-03-01 21:55:57 +0700

    added method to create component from object

--- src/main/java/dev/anhcraft/neep/struct/NeepComponent.java
+++ src/main/java/dev/anhcraft/neep/struct/NeepComponent.java
@@ -3,7 +3,53 @@ package dev.anhcraft.neep.struct;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
 public class NeepComponent {
+    @NotNull
+    public static NeepComponent create(@NotNull NeepContainer<?> container, @NotNull String key, @NotNull Object object) {
+        if(object instanceof NeepComponent) {
+            return (NeepComponent) object;
+        } else if(object instanceof String) {
+            return new NeepString(container, key, object.toString(), null);
+        } else if(object instanceof Boolean) {
+            return new NeepBoolean(container, key, object.toString(), null);
+        } else if(object instanceof Double || object instanceof Float) {
+            double val = ((Number) object).doubleValue();
+            return new NeepDouble(container, key, String.valueOf(val), null);
+        } else if(object instanceof Long) {
+            long val = (Long) object;
+            return new NeepLong(container, key, String.valueOf(val), null);
+        } else if(object instanceof Number) {
+            int val = ((Number) object).intValue();
+            return new NeepInt(container, key, String.valueOf(val), null);
+        } else if(object instanceof Collection) {
+            NeepList<NeepComponent> list = new NeepList<>(container, key, null, new ArrayList<>());
+            int i = 0;
+            for(Object o : (Collection<?>) object){
+                list.add(create(list, String.valueOf(i++), o));
+            }
+            return list;
+        } else if(object instanceof Map) {
+            NeepSection section = new NeepSection(container, key, null, new ArrayList<>());
+            for(Map.Entry<?, ?> entry : ((Map<?, ?>) object).entrySet()){
+                section.add(create(section, entry.getKey().toString(), entry.getValue()));
+            }
+            return section;
+        } else if(object.getClass().isArray()) {
+            NeepList<NeepComponent> list = new NeepList<>(container, key, null, new ArrayList<>());
+            int i = 0;
+            for(Object o : (Object[]) object){
+                list.add(create(list, String.valueOf(i++), o));
+            }
+            return list;
+        } else {
+            throw new IllegalArgumentException("Cannot convert object to component");
+        }
+    }
+
     private NeepContainer<?> parent;
 
     public NeepComponent(@Nullable NeepContainer<?> parent) {
